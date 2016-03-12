04b6311985d88c2e1195df6fe2864e8718d8aa68
Ricky Elrod <ricky@elrod.me>
2016-03-12 15:37:14 -0500

    Pretty up `pagure user <username>` output
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Pagure/Command/User.hs
+++ src/Pagure/Command/User.hs
@@ -3,6 +3,7 @@ module Pagure.Command.User where
 import Control.Lens
 import qualified Data.Text as T
 import Web.Pagure
+import Web.Pagure.Lens
 
 data UserOptions =
   UserOptions { userOptionsUsername :: String } deriving (Eq, Ord, Show)
@@ -10,6 +11,13 @@ data UserOptions =
 userCommandRunner :: UserOptions -> IO ()
 userCommandRunner (UserOptions username) = do
   let pc = PagureConfig "https://pagure.io" Nothing
-  pagureUser <- runPagureT (user (T.pack username)) pc
-  -- TODO: This output is very ugly
-  putStrLn (show pagureUser)
+  pagureUser <- runPagureT (userInfo (T.pack username)) pc
+  case pagureUser of
+    Nothing -> error $ "Unable to retrieve information for `" ++ username ++ "`"
+    Just userRes -> do
+      putStrLn $ "User      : " ++ userRes ^. user . name
+      putStrLn $ "Full Name : " ++ userRes ^. user . fullname
+      let repos' = userRes ^. repos
+          forks' = userRes ^. forks
+      putStrLn $ "# Repos   : " ++ show (length repos')
+      putStrLn $ "# Forks   : " ++ show (length forks')
